6b81288238f920b57f4eec1d5e8e9599e0528be8
raydanielg <airezra2+1@gmail.com>
2026-06-13 18:57:44 +0300

    commit

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name', 'description', 'price', 'original_price',
+        'color', 'rating', 'reviews', 'specs', 'category',
+        'image', 'is_featured', 'is_new', 'is_sale', 'is_active',
+    ];
+
+    protected $casts = [
+        'is_featured' => 'boolean',
+        'is_new'      => 'boolean',
+        'is_sale'     => 'boolean',
+        'is_active'   => 'boolean',
+        'price'       => 'float',
+        'original_price' => 'float',
+    ];
 }
